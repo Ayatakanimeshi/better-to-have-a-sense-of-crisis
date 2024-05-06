c2aba95de03808245dcd0c8f7feb68f6d3845ca2
Ayatakanimeshi <ayatakathebattler2top@gmail.com>
2024-05-06 21:28:53 +0900

    画像デプロイ準備

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,23 @@
 const { defineConfig } = require("@vue/cli-service");
+
 module.exports = defineConfig({
   transpileDependencies: true,
+  chainWebpack: (config) => {
+    // 画像ファイルに対する設定
+    config.module
+      .rule("images")
+      .use("url-loader")
+      .loader("url-loader")
+      .tap((options) => Object.assign(options || {}, { limit: 10240 }));
+
+    // ビデオファイルに対する設定
+    config.module
+      .rule("videos")
+      .test(/\.(mp4|webm|ogg|mp3|wav|flac|aac)(\?.*)?$/)
+      .use("file-loader")
+      .loader("file-loader")
+      .options({
+        name: "media/[name].[hash:8].[ext]",
+      });
+  },
 });
